de3c7ac543bd79554f97ce464633985d0d0fb720
Mario Muñoz Mejías <munozmejiasmario@proton.me>
2026-03-13 15:39:20 +0000

    Actualizar configuración de Firebase y mejorar la gestión de usuarios en la aplicación

--- ticketOS/src/firebase/client.js
+++ ticketOS/src/firebase/client.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Sustituye con las credenciales de tu proyecto en Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyC2jwIAXbafENVG5SLfKAVry7bYXup0CKg",
+  authDomain: "systicket-83627.firebaseapp.com",
+  projectId: "systicket-83627",
+   storageBucket: "systicket-83627.firebasestorage.app",
+  messagingSenderId: "637194630640",
+  appId: "1:637194630640:web:72c6777ce8f207bed55935"
 };
 
 const app = initializeApp(firebaseConfig);
